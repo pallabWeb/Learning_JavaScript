0c13980128d600276a889515949123c166833ec1
Pallab Mondal <157226684+pallabWeb@users.noreply.github.com>
2024-06-26 06:22:19 +0000

    add Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello Pallab")   // this is not work, we are using node.js, not browser
+
+let name = "pallab"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//unddfined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
