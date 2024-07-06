1d8698e078834b213076e94e37a79b22c593ad54
Pallab Mondal <pallabm2024@gmail.com>
2024-07-06 12:05:04 +0530

    conversion of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,26 @@
-let score = "fgh"
+let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-score = Number(score)
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "32abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "pallab" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
