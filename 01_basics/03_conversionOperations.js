2912be1c55ff9413d1b13278818905120983c545
Pallab Mondal <pallabm2024@gmail.com>
2024-07-06 12:23:03 +0530

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = undefined
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // "33" => 33
 // "32abc" => NaN
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,3 +24,40 @@ let someNumber = 44
 
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+// *************************************** Operations *************************************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " pallab"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameScore = 5
+++gameScore
+console.log(gameScore); // 6
