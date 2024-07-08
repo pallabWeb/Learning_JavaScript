dcfd54309a68e7424bfef18b032c6b5fae6c56aa
Pallab Mondal <pallabm2024@gmail.com>
2024-07-08 12:30:31 +0530

    add datatype_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+// Primitive
+
+// 7 types of primitive
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+
+// Reference (non-primitive)
+
+// 1. Object
+// 2. Array
+// 3. Functions
